afd400c1163043a6e53d0dd6db545fb83653faf1
YiChun Chuang <ss0940264@gmail.com>
2017-03-13 13:34:34 +0800

    Update Text Mining Assignment 1.py

--- Text Mining Assignment 1.py
+++ Text Mining Assignment 1.py
@@ -1,80 +1,32 @@
-
 # coding: utf-8
 
-# In[63]:
-
+# read file
 text = open('building_global_community.txt').read()
 
-
-# In[64]:
-
 get_ipython().system('pip3 install nltk -U')
-
-
-# In[65]:
 
 import nltk
 nltk.download('punkt')
 
-
-# In[159]:
-
 from nltk import wordpunct_tokenize
 
-
-# In[168]:
-
+# normalize words
 words = wordpunct_tokenize(text.lower())
-
-
-# In[206]:
-
+# filter out symbols
 words1 = [word for word in words if word.isalpha() is True]
 
-
-# In[207]:
-
+# filter out stopwords
 nltk.download('stopwords')
-
-
-# In[208]:
-
 from nltk.corpus import stopwords
 stopwords = set(stopwords.words('english'))
-
-
-# In[209]:
 
 import string
 string.punctuation
 stopwords.update(string.punctuation)
 
-
-# In[213]:
-
-# for word in words:
-#     if word not in stopwords:
-#         print(word)
 new_data = [word for word in words1 if word not in stopwords]
 
-
-# In[214]:
-
+# count the occurance of words  
 from collections import Counter
 counter = Counter(new_data)
-
-
-# In[215]:
-
 counter.most_common(20)
-
-
-# In[216]:
-
-
-
-
-# In[ ]:
-
-
-
